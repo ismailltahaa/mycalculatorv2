3803ae52e127d6d98b52ea661be043acf324bcf0
ismailltahaa <smailkcman@gmail.com>
2024-08-23 14:31:03 +0300

    ilk gönderim

---
+++ app/src/main/java/com/example/mycalculator2/CalculatorViewModel.kt
@@ -0,0 +1,48 @@
+package com.example.mycalculator2
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CalculatorViewModel : ViewModel() {
+
+    private val  _equationText = MutableLiveData("")
+    val  equationText : LiveData<String> = _equationText
+
+    private val _resultText = MutableLiveData ("")
+    val resultText : LiveData<String> = _resultText
+
+    fun onButtonClick(btn: String){
+        Log.i("Clicked Button",btn)
+
+        _equationText.value?.let{
+            if(btn=="AC"){
+                _equationText.value=""
+                _resultText.value="0"
+                return
+            }
+            if(btn=="C"){
+                if(it.isNotEmpty()){
+                    _equationText.value = it.substring(0,it.length-1)
+                    return
+                }
+
+            }
+
+            if(btn=="="){
+                _equationText.value= _resultText.value
+                return
+            }
+
+            _equationText.value= it+btn
+
+            //CALCULATOR RESULT
+
+            Log.i("Equation",_equationText.value.toString())
+
+
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mycalculator2/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.mycalculator2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModelProvider
+import com.example.mycalculator2.ui.theme.Mycalculator2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val calculatorViewModel =ViewModelProvider(this)[CalculatorViewModel::class.java]
+        enableEdgeToEdge()
+        setContent {
+            Mycalculator2Theme {
+                Box {
+                    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                        Calculator(modifier = Modifier.padding(innerPadding), calculatorViewModel)
+                    }
+                }
+            }
+        }
+    }
+}
+
